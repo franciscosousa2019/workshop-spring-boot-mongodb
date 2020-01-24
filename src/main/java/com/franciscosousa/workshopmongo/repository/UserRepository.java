4b91d985e860ab18c578a3ffc56993646a6edc90
Francisco Sousa <franciscosousa.fs2014@gmail.com>
2020-01-24 10:50:22 -0200

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/franciscosousa/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.franciscosousa.workshopmongo.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.franciscosousa.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+	
+
+}
