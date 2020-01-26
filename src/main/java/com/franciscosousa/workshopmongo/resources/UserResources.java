41ffcf215ab406a5d11c038e18cb08b68db216ae
Francisco Sousa <franciscosousa.fs2014@gmail.com>
2020-01-26 12:21:38 -0200

    Atualizacao de usuario com PUT

--- src/main/java/com/franciscosousa/workshopmongo/resources/UserResources.java
+++ src/main/java/com/franciscosousa/workshopmongo/resources/UserResources.java
@@ -52,4 +52,14 @@ public class UserResources {
     	return ResponseEntity.noContent().build();
     }
 	
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+    public ResponseEntity<User> update(@RequestBody UserDTO objDto, @PathVariable String id ) {
+    	User obj = service.fromDTO(objDto);
+    	obj.setId(id);
+    	obj = service.update(obj);
+    	return ResponseEntity.noContent().build();
+    }
+	
+	
+	
 }
